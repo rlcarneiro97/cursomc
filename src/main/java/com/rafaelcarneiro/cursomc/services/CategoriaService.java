7dc7742a4cd5c50b7a3c14fc6787dbc4b8f4c631
rlcarneiro97 <rafaeldelima97@hotmail.com>
2022-04-03 00:31:08 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/rafaelcarneiro/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.rafaelcarneiro.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.rafaelcarneiro.cursomc.domain.Categoria;
+import com.rafaelcarneiro.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id){
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
